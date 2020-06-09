6f01b476ca81666b9c49eaafe0c25a8466915c16
greattechpk <great.techpk@gmail.com>
2020-06-09 12:31:28 -0400

    added mern template and commiting first time dev enviorment implemented

---
+++ client/build/precache-manifest.8726cfb99751dacf6babfffb321c9404.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5465f8aea9bb0cfae4f75f003a19812",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f2710283176f68be60f",
+    "url": "/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "417350073b77e676352b",
+    "url": "/static/js/2.188968b9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.188968b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f2710283176f68be60f",
+    "url": "/static/js/main.db6227c7.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
