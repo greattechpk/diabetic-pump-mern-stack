4c550edba4f43873ae226d0a3e138ddefffd25ff
greattechpk <great.techpk@gmail.com>
2020-06-10 13:02:29 -0400

    added reactivity to the create insuling form creating sub mit and adding math . floor to labels and ranges prepped for api implementtation

--- client/src/components/NewInsulin.js
+++ client/src/components/NewInsulin.js
@@ -17,7 +17,8 @@ export default class NewInsulin extends Component {
             carbRatio: 0,
             correctionSubtract: 0,
             correctionDivisor: 0,
-            insulinType: ''
+            insulinType: '',
+            activeInsulin: 0
         }
     }
 
@@ -36,6 +37,50 @@ export default class NewInsulin extends Component {
         }
     }
 
+    onChangeNum = (evt) => {
+        const newState = { ...this.state }
+        newState.form[evt.target.name] = parseInt(evt.target.value)
+        
+        this.setState(newState)
+        if(evt.target.name === "bloodGlucose"){
+            this.calcBg()
+            this.calcTotal()
+        }
+        if(evt.target.name === "totalCarbs"){
+            this.calcFood()
+            this.calcTotal()
+        }
+        console.log(this.state)
+    }
+
+    calcFood = () => {
+        const tCarbs = this.state.form.totalCarbs
+        const newState = {...this.state}
+        newState.form.totalFoodDelivery = tCarbs / this.state.settings.carbRatio
+        document.getElementById('totalFoodDelivery').textContent = newState.form.totalFoodDelivery
+        this.setState(newState)
+    }
+
+    calcBg = () => {
+        const bg = this.state.form.bloodGlucose
+        let tc = this.state.form.totalCorrection
+        let fc = this.state.form.fixedCorrection
+        const newState = {...this.state}
+        tc = (bg - this.state.settings.correctionSubtract) / this.state.settings.correctionDivisor
+        fc = tc - this.state.settings.activeInsulin
+        document.getElementById('totalCorrection').textContent = tc
+        document.getElementById('fixedCorrection').textContent = fc
+        newState.form.totalCorrection = tc
+        newState.form.fixedCorrection = fc
+        this.setState(newState)
+    }
+    calcTotal = () => {
+        const newState = {...this.state}
+        newState.form.totalDelivery = newState.form.fixedCorrection + newState.form.totalFoodDelivery
+        document.getElementById('totalDelivery').textContent = newState.form.totalDelivery
+        this.setState(newState)
+    }
+
     render() {
         return (
             <div>
@@ -43,24 +88,24 @@ export default class NewInsulin extends Component {
                     <h3>Correction</h3>
                     <div className='form-group'>
                         <label htmlFor='bloodGlucose'>Blood Glucose</label>
-                        <input type="number" name="bloodGlucose" />
+                        <input type="number" name="bloodGlucose" onChange={this.onChangeNum}/>
                     </div>
                     <div className='form-group'>
-                        <span className='form-entry-label'>Fixed Correction</span>
-                        <span id='fixedCorrection' className='automated'></span>
+                        <span className='form-entry-label'>Total Correction:</span>
+                        <span id='totalCorrection' className='automated'>0</span>
                     </div>
                     <div className='form-group'>
-                        <span className='form-entry-label'>Total Correction</span>
-                        <span id='totalCorrection' className='automated'></span>
+                        <span className='form-entry-label'>Fixed Correction:</span>
+                        <span id='fixedCorrection' className='automated'>0</span>
                     </div>
                     <h3>Carbs</h3>
                     <div className='form-group'>
                         <label htmlFor='totalCarbs'>Total Carbs</label>
-                        <input type="number" name="totalCarbs" />
+                        <input type="number" name="totalCarbs" onChange={this.onChangeNum}/>
                     </div>
                     <div className='form-group'>
-                        <span className='form-entry-label'>Total Food Delivery</span>
-                        <span id='totalFoodDelivery' className='automated'></span>
+                        <span className='form-entry-label'>Total Food Delivery:</span>
+                        <span id='totalFoodDelivery' className='automated'>0</span>
                     </div>
                     <h3>Delivery Summary</h3>
                     <div className='form-group'>
@@ -71,8 +116,8 @@ export default class NewInsulin extends Component {
                         </select>
                     </div>
                     <div className='form-group'>
-                        <span className='form-entry-label'>Total Delivery</span>
-                        <span id='totalDelivery' className='automated'></span>
+                        <span className='form-entry-label'>Total Delivery:</span>
+                        <span id='totalDelivery' className='automated'>0</span>
                     </div>
                     <input type="submit" value="Create" />
                 </form>
